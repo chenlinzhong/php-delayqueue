0a97e78fabe229b0d921a92350f81b8894087465
linzhong <490103404@qq.com>
2018-10-13 19:38:39 +0000

    配置文件

--- DqConf.php
+++ DqConf.php
@@ -8,7 +8,7 @@ class DqConf{
         'host'=>'127.0.0.1',
         'port'=>'3306',
         'user'=>'user',
-        'password'=>'xx',
+        'password'=>'xx',   
         'database'=>'dq',
     );
 
