8223558d83d22cc27f948c3da11834a8515d084c
linzhong <linzhong@staff.weibo.com>
2018-11-09 23:15:50 +0800

    meta数据sql

---
+++ dq.sql
@@ -0,0 +1,33 @@
+create database dq;
+#存放告警信息
+CREATE TABLE `dq_alert` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host` varchar(255) NOT NULL DEFAULT '',
+  `port` int(11) NOT NULL DEFAULT '0',
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `pwd` varchar(255) NOT NULL DEFAULT '',
+  `ext` varchar(2048) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+#存放redis信息
+CREATE TABLE `dq_redis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_name` varchar(200) NOT NULL DEFAULT '',
+  `t_content` varchar(2048) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+#存储注册信息
+CREATE TABLE `dq_topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_name` varchar(1024) NOT NULL DEFAULT '',
+  `delay` int(11) NOT NULL DEFAULT '0',
+  `callback` varchar(1024) NOT NULL DEFAULT '',
+  `timeout` int(11) NOT NULL DEFAULT '3000',
+  `email` varchar(1024) NOT NULL DEFAULT '',
+  `topic` varchar(255) NOT NULL DEFAULT '',
+  `re_notify_flag` varchar(1024) NOT NULL DEFAULT '重试标记',
+  `createor` varchar(1024) NOT NULL DEFAULT '',
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `method` varchar(32) NOT NULL DEFAULT 'GET',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
