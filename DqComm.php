413c546dc4094f6172c22a53a5007c637df79129
linzhong <linzhong@staff.weibo.com>
2018-12-15 12:06:16 +0800

    日志优化

--- DqComm.php
+++ DqComm.php
@@ -8,7 +8,7 @@ class DqComm{
     public static function socket_read($cfd){
         try {
             if (is_resource($cfd)) {
-                $msg_len = socket_read($cfd, 4); //读取数据前四个字节,表示本条消息的长度
+                $msg_len = @socket_read($cfd, 4); //读取数据前四个字节,表示本条消息的长度
                 if($msg_len===false){
                     $strMsg =  socket_strerror(socket_last_error($cfd));
                     throw  new DqException('read error,msg='.$strMsg);
@@ -50,9 +50,9 @@ class DqComm{
                 DqLog::writeLog('data too long,str=' . $data . ' max_package_size=' . self::$max_package_size,DqLog::LOG_TYPE_EXCEPTION);
             }
             while ($len) {
-                $nwrite = socket_write($fd, $data, $len); //可能一次性写不完，需要多次写入
+                $nwrite = @socket_write($fd, $data, $len); //可能一次性写不完，需要多次写入
                 if ($nwrite === false) { /*数据写入失败*/
-                    DqLog::writeLog('socker error: data=' . $data . ',fd=' . $fd . ',error=' . error_get_last(),DqLog::LOG_TYPE_EXCEPTION);
+                    DqLog::writeLog('socker error: data=' . $data . ',fd=' . $fd . ',error=' . json_encode(error_get_last()),DqLog::LOG_TYPE_EXCEPTION);
                     return false;
                 } else if ($nwrite > 0) {
                     $len -= $nwrite;
